c8e95c4a14d312fc9e0ce1cd8a1fc8b553e5275e
Dhanush S <dhanushaarnit143@gmail.com>
2026-09-09 08:15:04 +0530

    Time: 0 ms (100%), Space: 12.4 MB (30%) - LeetHub

---
+++ 4248-count-commas-in-range-ii/4248-count-commas-in-range-ii.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def countCommas(self, n):
+        ans = 0
+        limit = 999
+        
+        while limit < n:
+            ans += n - limit
+            limit = limit * 1000 + 999
+            
+        return ans
